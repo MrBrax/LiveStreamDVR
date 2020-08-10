619b04a7a12e8ad47e719df231a30b393ba664a7
braxen <braxen@braxnet.org>
2020-08-10 10:49:42 +0200

    move css to assets folder, make hook messages debug

--- src/Controller/HookController.php
+++ src/Controller/HookController.php
@@ -14,7 +14,7 @@ class HookController
     public function hook( Request $request, Response $response, $args )
     {
 
-        TwitchHelper::log( TwitchHelper::LOG_INFO, "Hook called" );
+        TwitchHelper::log( TwitchHelper::LOG_DEBUG, "Hook called" );
 
         $TwitchAutomator = new TwitchAutomator();
 
@@ -35,13 +35,13 @@ class HookController
         $post_json = isset( $_POST['json'] ) ? $_POST['json'] : null;
 
         if ($post_json) {
-            TwitchHelper::log( TwitchHelper::LOG_INFO, "Custom payload received..." );
+            TwitchHelper::log( TwitchHelper::LOG_DEBUG, "Custom payload received..." );
             $data = json_decode($post_json, true);
         }
 
         if ($data) {
 
-            TwitchHelper::log( TwitchHelper::LOG_INFO, "Dumping payload..." );
+            TwitchHelper::log( TwitchHelper::LOG_DEBUG, "Dumping payload..." );
             file_put_contents( __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 'payloads' . DIRECTORY_SEPARATOR . date("Y-m-d.h_i_s") . '.json', json_encode($data) );
             
             /*
@@ -52,12 +52,12 @@ class HookController
             $data_username = $data['data'][0]['user_name'];
             */
 
-            TwitchHelper::log( TwitchHelper::LOG_INFO, "Run handle..." );
+            TwitchHelper::log( TwitchHelper::LOG_DEBUG, "Run handle..." );
             $TwitchAutomator->handle($data);
 
         } else {
             
-            TwitchHelper::log( TwitchHelper::LOG_INFO, "Hook called with no data..." );
+            TwitchHelper::log( TwitchHelper::LOG_WARNING, "Hook called with no data..." );
             $response->getBody()->write("No data supplied");
 
         }
