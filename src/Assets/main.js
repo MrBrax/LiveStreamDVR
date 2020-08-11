b9b0205a93784dfec617064f99160caf030de956
Braxen <braxen@braxnet.org>
2020-08-11 22:31:52 +0200

    live colour fix

--- src/Assets/main.js
+++ src/Assets/main.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    console.log("Loaded");
-
     let api_base = window.base_path + "/api";
 
+    let delay = 120;
+
     function updateStreamers(){
+
+        console.log(`Fetching streamer list (${delay})...`);
+
+        let any_live = false;
 
         fetch( api_base + "/list").then(response => response.json()).then(data => {
             if( data.data ){
                 for( let streamer of data.data.streamerList ){
-                    console.log( streamer );
+                    // console.log( streamer );
                     let menu = document.querySelector(".top-menu-item.streamer[data-streamer='" + streamer.username + "']");
                     let subtitle = menu.querySelector(".subtitle");
                     let vodcount = menu.querySelector(".vodcount");
                     vodcount.innerHTML = streamer.vods_list.length;
                     if( streamer.is_live ){
+                        any_live = true;
                         menu.classList.add('live');
                         // subtitle.innerHTML = 'Live';
                         subtitle.innerHTML = 'Playing <strong>' + streamer.current_game.game_name + '</strong>';
@@ -23,14 +28,22 @@ document.addEventListener("DOMContentLoaded", () => {
                         menu.classList.remove('live');
                     }
                 }
+                if(any_live){
+                    delay = 120;
+                }else{
+                    delay += 10
+                }
+                setTimeout(updateStreamers, delay * 1000);
             }
         });
 
+        
+
     }
 
-    // updateStreamers();
+    updateStreamers();
 
-    setInterval(updateStreamers, 60 * 1000);
+    
 
 });
 
