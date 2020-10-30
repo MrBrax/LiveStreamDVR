9c325e5db2bf212df3f5661841a0264cba96a5c0
Braxen <braxen@braxnet.org>
2020-10-30 21:58:41 +0100

    Update twitch-chat.py

--- src/Utilities/twitch-chat.py
+++ src/Utilities/twitch-chat.py
@@ -12,6 +12,11 @@ import select
 
 # twitch-chat.py CHANNEL USERID OUTPUT
 
+# config
+block_size = 128
+time_to_save = 120
+
+
 Twitch_user = "justinfan{random}".format(random=int(random()*9999999))
 Twitch_pass = "blah"
 Twitch_channel = sys.argv[1]
@@ -19,22 +24,30 @@ Twitch_userid = sys.argv[2]
 chat_log_path = "./"
 sub = False
 sub_time = None
-
-num_to_save = 500
-last_save = 0
-
 output_path = sys.argv[3]
-
-raw_text = ""
-
 running = True
-
-dateformat = "%Y-%m-%dT%H:%M:%SZ"
-
+dateformat = "%Y-%m-%dT%H:%M:%S.%fZ"
 time_start = time.time()
 date_start = datetime.datetime.utcnow().strftime(dateformat)
-
 ls_cap = True
+last_save = 0
+
+irc_newline = "\r\n"
+
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+raw_text = ""
 
 jsondata = {
     "comments": [],
@@ -114,7 +127,7 @@ serv_prog = re.compile( r"@(?P<tags>.*)\s\:tmi\.twitch\.tv\s(?P<action>[A-Z]+)\s
 def saveJSON():
     with open(output_path, 'w') as outfile:
 
-        print("Saving JSON...")
+        print( bcolors.OKGREEN + "Saving JSON..." + bcolors.ENDC )
 
         time_duration = time.time() - time_start
         hours, remainder = divmod(time_duration, 3600)
@@ -125,9 +138,9 @@ def saveJSON():
         jsondata['video']['duration'] = total_duration
         json.dump(jsondata, outfile)
 
-        print("JSON saved, hopefully!")
+        print( bcolors.OKGREEN + "JSON saved, hopefully!" + bcolors.ENDC )
     
-    print("Saving raw txt...")
+    print( bcolors.OKGREEN + "Saving raw txt..." + bcolors.ENDC )
     outfile = open(output_path + ".txt", 'w+', encoding='utf-8')
     outfile.write(raw_text)
     outfile.close()
@@ -183,230 +196,279 @@ def parse_badges( raw_data ):
 
     return badges
 
-while( True ):
+# DaCurse
+def recv_until(conn, prev, delim=b'\r\n', block_size=1024):
+    buff = b''
+    while delim not in buff:
+        # print("Read data...")
+        buff += conn.recv(block_size)
 
-    if not running:
-        break
+    delim_pos = buff.index(delim)
+    data = (prev if prev else b'') + buff[:delim_pos]
+    extra = buff[delim_pos + len(delim):]
 
-    ready = select.select([irc], [], [], 10)
+    return data, extra
 
-    buff_raw = b''
+def process_buffer( irc, buff_raw ):
 
-    if ready[0]:
-        buff_raw += irc.recv( 1024 )
-    else:
-        print("No response, retrying...")
-        continue
-
-    # while True:
-    #     if not running:
-    #         break
-    #     newdata = irc.recv( 1024 )
-    #     if not newdata:
-    #         break
-    #     buff_raw += newdata
+    global raw_text
+    global last_save
+    global jsondata
+    global time_to_save
 
     try:
         buff_utf = buff_raw.decode('utf-8')
     except UnicodeDecodeError as err:
         print("Couldn't decode packet (" + str(err) + "): ", buff_raw)
-        continue
-        pass
+        return False
     
+    if not buff_raw:
+        print("Nothing to process")
+        return False
+
     # print( "Buffer >> ", buff_utf )
     # print( "" )
 
-    print(" >> Buff >> " + buff_utf[0:160] )
+    # print(" >> Buff >> " + buff_utf[0:160] )
 
-    buff_split = buff_utf.split("\n")
+    buf = buff_utf
+    # buff_split = buff_utf.split("\r\n")
     # print("Buffer newlines: " + str( len( buff_split ) ) )
-    for buf in buff_split[:-1]:
+    # for buf in buff_split[:-1]:
 
-        # print(" >> Line >> " + buf)
-        # print(" >> Line >> " + buf[0:160] )
+    # print(" >> Line >> " + buf)
+    # print(" >> Line >> " + buf[0:160] )
 
-        #to keep the connection alive, we have to reply to the ping
-        #Twitch terminates the connection if it doesn't receive a reply after 3 pings
+    #to keep the connection alive, we have to reply to the ping
+    #Twitch terminates the connection if it doesn't receive a reply after 3 pings
+    
+    if "PING" in buf:
+        print( bcolors.OKBLUE + ">> Received ping" + bcolors.ENDC )
+        buf = str.encode("PONG tmi.twitch.tv\r\n")
+        irc.send(buf)
+        return False
+
+    if buf == ":tmi.twitch.tv RECONNECT":
+        print( bcolors.OKBLUE + ">> Received reconnect!" + bcolors.ENDC )
+        irc = connect()
+        return False
+
+    # if buf[0] != "@" and buf[0] != ":":
+    #      print( bcolors.FAIL + "Corrupt >> " + str(buf) + bcolors.ENDC )
+    #      print( "" )
+    
+    
+    # privmsg_data = privmsg_prog.match( buf )
+    # clearchat_data = clearchat_prog.match( buf )
+
+    irc_action = None
+
+    action_data = action_prog.match( buf )
+    if action_data:
+        irc_tags = action_data.group("tags")
+        irc_user = action_data.group("user")
+        irc_action = action_data.group("action")
+        irc_channel = action_data.group("channel")
+        irc_message = action_data.group("message")
+    else:
+        serv_data = serv_prog.match( buf )
+        if serv_data:
+            irc_tags = serv_data.group("tags")
+            # irc_user = serv_data.group("user")
+            irc_action = serv_data.group("action")
+            irc_channel = serv_data.group("channel")
+            irc_message = serv_data.group("message")
+
+    
+        # print(irc_user, irc_action, irc_channel, irc_message)
+    
+    if irc_action == "PRIVMSG":
+
+        offset = time.time() - time_start
+        now = datetime.datetime.utcnow()
+
+        body_text = irc_message
+
+        # unsure how to handle this yet
+        body_text = body_text.replace("\u0001ACTION", "")
+        body_text = body_text.replace("\x01", "")
+        body_text = body_text.strip()
+
+        tags = None
+
+        if irc_tags:
+
+            tags = parse_irc_tags( irc_tags )
         
-        if "PING" in buf:
-            print(">> Received ping")
-            buf = str.encode("PONG tmi.twitch.tv\r\n")
-            irc.send(buf)
-            continue
+            emoticons = parse_emoticons( tags['emotes'] )
 
-        if buf == ":tmi.twitch.tv RECONNECT":
-            print(">> Received reconnect!")
-            irc = connect()
-            continue
-
-        # if buf[0] != "@":
-        #     print( "Corrupt >> " + str(buf) )
-        #     print( "" )
-        
-        
-        # privmsg_data = privmsg_prog.match( buf )
-        # clearchat_data = clearchat_prog.match( buf )
-
-        irc_action = None
-
-        action_data = action_prog.match( buf )
-        if action_data:
-            irc_tags = action_data.group("tags")
-            irc_user = action_data.group("user")
-            irc_action = action_data.group("action")
-            irc_channel = action_data.group("channel")
-            irc_message = action_data.group("message")
-        else:
-            serv_data = serv_prog.match( buf )
-            if serv_data:
-                irc_tags = serv_data.group("tags")
-                # irc_user = serv_data.group("user")
-                irc_action = serv_data.group("action")
-                irc_channel = serv_data.group("channel")
-                irc_message = serv_data.group("message")
-
-        
-            # print(irc_user, irc_action, irc_channel, irc_message)
-        
-        if irc_action == "PRIVMSG":
-
-            offset = time.time() - time_start
-            now = datetime.datetime.utcnow()
-
-            body_text = irc_message
-
-            # unsure how to handle this yet
-            body_text = body_text.replace("\u0001ACTION", "")
-            body_text = body_text.replace("\x01", "")
-            body_text = body_text.strip()
-
-            tags = None
-
-            if irc_tags:
-
-                tags = parse_irc_tags( irc_tags )
-           
-                emoticons = parse_emoticons( tags['emotes'] )
-
-                badges = parse_badges( tags['badges'] )
-                
-                fragments = []
-
-                text_buffer = ""
-
-                if emoticons:
-
-                    for i, letter in enumerate(body_text):
-                        
-                        text_buffer += letter
-                        
-                        has_emote = False
-                        
-                        for emote in emoticons:
-                            
-                            #if i >= emote['begin'] and i <= emote['end']:
-                            #    has_emote = True
-                            
-                            if i+1 == emote['begin']:
-                                # text node
-                                fragments.append({
-                                    "text": text_buffer
-                                })
-                                # print("Append text: " + text_buffer)
-                                text_buffer = ""
-                            
-                            if i == emote['end']:
-                                # emoticon node
-                                fragments.append({
-                                    "emoticon": {
-                                        "emoticon_id": emote["_id"]
-                                    },
-                                    "text": text_buffer
-                                })
-                                # print("Append emoticon: " + text_buffer)
-                                text_buffer = ""
+            badges = parse_badges( tags['badges'] )
             
-                else:
-                    fragments.append({
-                        "text": body_text
-                    })
+            fragments = []
+
+            text_buffer = ""
+
+            if emoticons:
+
+                for i, letter in enumerate(body_text):
+                    
+                    text_buffer += letter
+                    
+                    for emote in emoticons:
+                        
+                        #if i >= emote['begin'] and i <= emote['end']:
+                        #    has_emote = True
+                        
+                        if i+1 == emote['begin']:
+                            # text node
+                            fragments.append({
+                                "text": text_buffer
+                            })
+                            # print("Append text: " + text_buffer)
+                            text_buffer = ""
+                        
+                        if i == emote['end']:
+                            # emoticon node
+                            fragments.append({
+                                "emoticon": {
+                                    "emoticon_id": emote["_id"]
+                                },
+                                "text": text_buffer
+                            })
+                            # print("Append emoticon: " + text_buffer)
+                            text_buffer = ""
         
-            comment = {
-                "commenter": {
-                    "_id": tags["user-id"],
-                    "bio": None, # fake
-                    "created_at": now.strftime( dateformat ),
-                    "display_name": tags["display-name"],
-                    "logo": None, # fake
-                    "name": irc_user,
-                    "type": "user", # fake
-                    "updated_at": now.strftime( dateformat )
-                },
-                "message": {
-                    "body": body_text,
-                    "emoticons": emoticons,
-                    "fragments": fragments,
-                    "is_action": False, # fake
-                    "user_badges": badges,
-                    "user_color": tags["color"] or "#FFFFFF",
-                    "user_notice_params": {} # fake
-                },
-                "source": "chat", # fake
-                "state": "published", # fake
-                "content_offset_seconds": round(offset, 4),
+            else:
+                fragments.append({
+                    "text": body_text
+                })
+    
+        comment = {
+            "commenter": {
+                "_id": tags["user-id"],
+                "bio": None, # fake
                 "created_at": now.strftime( dateformat ),
+                "display_name": tags["display-name"],
+                "logo": None, # fake
+                "name": irc_user,
+                "type": "user", # fake
                 "updated_at": now.strftime( dateformat )
-            }
-            
-            jsondata['comments'].append( comment )
-            # print( "Message >> " + comment['commenter']['name'] + ": " + comment['message']['body'] )
-            raw_text += "<{date}> {user}: {message}\n".format( date=now.strftime(dateformat), user=tags["display-name"], message=body_text )
-            
-            # num_since_saved += 1
-            # if num_since_saved > num_to_save:
-            #     print("Reached {num} messages, saving just to be sure!".format(num=num_to_save))
-            #     num_since_saved = 0
-            #     saveJSON()
-            if time.time() > last_save + 60:
-                print("Saving after one minute just to be sure!")
-                last_save = time.time()
-                saveJSON()
-
-            # print( " Emotes: ", emoticons )
-            # print( " Badges: ", badges )
-            # print( " Fragments: ", fragments )
-            # print( "" )
-            # chat_log.write("{time}{user}:{message}\r\n".format(,,))
+            },
+            "message": {
+                "body": body_text,
+                "emoticons": emoticons,
+                "fragments": fragments,
+                "is_action": False, # fake
+                "user_badges": badges,
+                "user_color": tags["color"] or "#FFFFFF",
+                "user_notice_params": {} # fake
+            },
+            "source": "chat", # fake
+            "state": "published", # fake
+            "content_offset_seconds": round(offset, 4),
+            "created_at": now.strftime( dateformat ),
+            "updated_at": now.strftime( dateformat )
+        }
         
-        #elif clearchat_data:
-        #    
-        #    tags = {}
-        #    raw_tags = clearchat_data.group(1).split(";")
-        #    for tag in raw_tags:
-        #        tags[ tag.split("=")[0] ] = tag.split("=")[1]
-        #        # print( "Tag: " + key + " = " + value )
-        #
-        #    print( "Clear chat >> ", tags)
+        jsondata['comments'].append( comment )
+
+        cmd_message = comment['message']['body']
+        cmd_message = re.sub(r'(\@\w+)\s', bcolors.FAIL + bcolors.BOLD + r'\1 ' + bcolors.ENDC, cmd_message )
+        print( "<{date}> {user}: {message}".format( date=bcolors.HEADER+now.strftime(dateformat), user=bcolors.OKBLUE + comment['commenter']['display_name'], message=bcolors.ENDC+cmd_message ) )
+        raw_text += "<{date}> {user}: {message}\n".format( date=now.strftime(dateformat), user=comment['commenter']['display_name'], message=body_text )
         
-        elif irc_action == "ROOMSTATE":
-            print( "Room state >> ", irc_message )
-        elif irc_action == "USERNOTICE":
-            print( "User notice >> ", irc_user, irc_message )
-        elif irc_action == "CLEARCHAT":
-            print( "Clear chat >> ", irc_user, irc_message )
-        # else:
-        #     print( "Unhandled >> " + buf )
+        # num_since_saved += 1
+        # if num_since_saved > num_to_save:
+        #     print("Reached {num} messages, saving just to be sure!".format(num=num_to_save))
+        #     num_since_saved = 0
+        #     saveJSON()
+        if time.time() > last_save + time_to_save:
+            print( bcolors.OKGREEN + "Saving after one minute just to be sure!" + bcolors.ENDC )
+            last_save = time.time()
+            saveJSON()
 
-        # except:
-        #     try:
-        #         with open(chat_log_path+"Twitch_logger.txt","a+") as chat_log:
-        #             print( "Info: " + buf )
-        #             chat_log.write("{time} - {message}\r\n".format( time=datetime.date.today().strftime(dateformat), message=buf ))
-        #     #if there is an error, this will print exactly what went wrong, and just continue if possible, because of the while loop
-        #     #Without Try/Except the script would stop completely
-        #     except Exception as error:
-        #         print(error)
+        # print( " Emotes: ", emoticons )
+        # print( " Badges: ", badges )
+        # print( " Fragments: ", fragments )
+        # print( "" )
+        # chat_log.write("{time}{user}:{message}\r\n".format(,,))
+    
+    #elif clearchat_data:
+    #    
+    #    tags = {}
+    #    raw_tags = clearchat_data.group(1).split(";")
+    #    for tag in raw_tags:
+    #        tags[ tag.split("=")[0] ] = tag.split("=")[1]
+    #        # print( "Tag: " + key + " = " + value )
+    #
+    #    print( "Clear chat >> ", tags)
+    
+    elif irc_action == "ROOMSTATE":
+        print( bcolors.OKCYAN + "Room state >> ", buf, bcolors.ENDC )
+    elif irc_action == "USERNOTICE":
+        print( bcolors.OKCYAN + "User notice >> ", buf, bcolors.ENDC )
+    elif irc_action == "CLEARCHAT":
+        # tags = parse_irc_tags( irc_tags )
+        # print( bcolors.FAIL, irc_user, "got banned for", tags['ban-duration'], "minutes!", bcolors.ENDC )
+        print( bcolors.OKCYAN + "Clear chat >> ", buf, bcolors.ENDC )
+    # else:
+    #    print( bcolors.FAIL + "Unhandled >> " + buf + bcolors.ENDC )
 
+    # except:
+    #     try:
+    #         with open(chat_log_path+"Twitch_logger.txt","a+") as chat_log:
+    #             print( "Info: " + buf )
+    #             chat_log.write("{time} - {message}\r\n".format( time=datetime.date.today().strftime(dateformat), message=buf ))
+    #     #if there is an error, this will print exactly what went wrong, and just continue if possible, because of the while loop
+    #     #Without Try/Except the script would stop completely
+    #     except Exception as error:
+    #         print(error)
+
+extra = None
+
+while( True ):
+
+    if not running:
+        break
+        
+    '''
+    buff_raw = b''
+
+    while True:
+
+        if not running:
+            break
+
+        # print( buff_raw[-1:] )
+
+        ready = select.select([irc], [], [], 10)    
+
+        if ready[0]:
+            # print("Read 1024...")
+            buff_raw += irc.recv( block_size )
+
+            if b'\r\n' in buff_raw:
+                # print("Found newline")
+                break
+
+        else:
+            print("No response, retrying...")
+            continue
+    '''
+    
+    ready = select.select([irc], [], [], 10)
+
+    buff_raw = b''
+
+    if ready[0]:
+        buff_raw, extra = recv_until(irc, extra, block_size=block_size)
+        # print("Process buffer...")
+        process_buffer(irc, buff_raw)
+    else:
+        print("No response, retrying...")
+
+    # process_buffer( buff_raw )
+    
 print("Exited loop...")
 
 saveJSON()
