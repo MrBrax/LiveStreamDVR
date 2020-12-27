2934add8d7cea46a10c079e35ec01ca015cca55c
Braxen <braxen@braxnet.org>
2020-12-27 01:50:13 +0100

    youtube testing

--- src/Controller/HookController.php
+++ src/Controller/HookController.php
@@ -19,9 +19,9 @@ class HookController
 
         TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Hook called");
 
-        $TwitchAutomator = new TwitchAutomator();
-
         set_time_limit(0);
+
+        $source = isset($_GET['source']) ? $_GET['source'] : 'twitch';
 
         // handle hub challenge after subscribing
         if (isset($_GET['hub_challenge'])) {
@@ -29,60 +29,67 @@ class HookController
             $challenge_token = $_GET['hub_challenge'];
 
             $user_id = null;
-            if (isset($_GET['hub_topic'])) {
+            if ($source == 'twitch') {  // twitch parse channel id
+                if (isset($_GET['hub_topic'])) {
+                    $user_url = parse_url($_GET['hub_topic']);
+                    parse_str($user_url['query'], $user_query);
+                    if (isset($user_query['user_id'])) {
+                        $user_id = $user_query['user_id'];
+                    }
+                }
+            } elseif ($source == 'youtube') { // youtube parse channel id
                 $user_url = parse_url($_GET['hub_topic']);
                 parse_str($user_url['query'], $user_query);
-                if (isset($user_query['user_id'])) {
-                    $user_id = $user_query['user_id'];
+                if (isset($user_query['channel_id'])) {
+                    $user_id = $user_query['channel_id'];
                 }
             }
 
             $hub_reason = isset($_GET['hub_reason']) ? $_GET['hub_reason'] : null;
 
             if (isset($hub_reason)) {
-                TwitchHelper::logAdvanced(TwitchHelper::LOG_ERROR, "hook", "Received hub challenge with error for userid {$user_id}: {$hub_reason}", ['GET' => $_GET, 'POST' => $_POST, 'user_id' => $user_id]);
+                TwitchHelper::logAdvanced(TwitchHelper::LOG_ERROR, "hook", "Received hub challenge ({$source}) with error for userid {$user_id}: {$hub_reason}", ['GET' => $_GET, 'POST' => $_POST, 'user_id' => $user_id]);
             } else {
-                TwitchHelper::logAdvanced(TwitchHelper::LOG_SUCCESS, "hook", "Received hub challenge for userid {$user_id}", ['GET' => $_GET, 'POST' => $_POST, 'user_id' => $user_id]);
+                TwitchHelper::logAdvanced(TwitchHelper::LOG_SUCCESS, "hook", "Received hub challenge ({$source}) for userid {$user_id}", ['GET' => $_GET, 'POST' => $_POST, 'user_id' => $user_id]);
             }
 
+            // just write the response back without checking anything
             $response->getBody()->write($challenge_token);
 
             return $response;
         }
 
-        
+
         // handle regular hook
-        $data = json_decode(file_get_contents('php://input'), true);
 
-        $post_json = isset($_POST['json']) ? $_POST['json'] : null;
-
-        if ($post_json) {
-            TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Custom payload received...");
-            $data = json_decode($post_json, true);
-        }
-
-        if ($data) {
-
-            if (TwitchConfig::cfg('debug')) {
-                TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Dumping payload...");
-                file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 'payloads' . DIRECTORY_SEPARATOR . date("Y-m-d.h_i_s") . '.json', json_encode($data));
+        if ($source == 'twitch') {
+            $data_json = json_decode(file_get_contents('php://input'), true);
+            $post_json = isset($_POST['json']) ? $_POST['json'] : null;
+            if ($post_json) {
+                TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Custom payload received...");
+                $data_json = json_decode($post_json, true);
             }
 
-            /*
-            $data_id = $data['data'][0]['id'];
-            $data_title = $data['data'][0]['title'];
-            $data_started = $data['data'][0]['started_at'];
-            $data_game_id = $data['data'][0]['game_id'];
-            $data_username = $data['data'][0]['user_name'];
-            */
+            if ($data_json) {
 
-            TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Run handle...");
-            $TwitchAutomator->handle($data);
-        } else {
+                if (TwitchConfig::cfg('debug')) {
+                    TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Dumping payload...");
+                    file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 'payloads' . DIRECTORY_SEPARATOR . date("Y-m-d.h_i_s") . '.json', json_encode($data_json));
+                }
 
-            TwitchHelper::logAdvanced(TwitchHelper::LOG_WARNING, "hook", "Hook called with no data...");
-            $response->getBody()->write("No data supplied");
+                TwitchHelper::logAdvanced(TwitchHelper::LOG_DEBUG, "hook", "Run handle...");
+                $TwitchAutomator = new TwitchAutomator();
+                $TwitchAutomator->handle($data_json);
+                return $response;
+            }
+        } elseif ($source == 'youtube') {
+            $data_xml = simplexml_load_string(file_get_contents('php://input'), true);
+            TwitchHelper::logAdvanced(TwitchHelper::LOG_INFO, "hook", "Hook called YouTube data...", ['xml' => $data_xml, 'GET' => $_GET, 'POST' => $_POST]);
+            return $response;
         }
+
+        TwitchHelper::logAdvanced(TwitchHelper::LOG_WARNING, "hook", "Hook called with no data ({$source})...");
+        $response->getBody()->write("No data supplied");
 
         return $response;
     }
