1a19557ff96d1129d5e07c5442fabc2437bc0242
braxen <braxen@braxnet.org>
2022-05-03 13:25:08 +0200

    set stream number on new created vod

--- server/src/Core/Automator.ts
+++ server/src/Core/Automator.ts
@@ -591,6 +591,10 @@ export class Automator {
         this.vod.capture_id = this.getVodID() || "1";
         this.vod.started_at = parseJSON(data_started);
 
+        this.vod.stream_number = this.channel.current_stream_number;
+        this.channel.current_stream_number++;
+        KeyValue.getInstance().setInt(`${this.broadcaster_user_login}.stream_number`, this.channel.current_stream_number);
+
         if (this.force_record) this.vod.force_record = true;
 
         this.vod.not_started = false;
