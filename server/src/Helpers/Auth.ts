0bdb34de03e0a94cbafd889e754a06a9ef6d4235
CATERPILLAR\Braxen <braxen@braxnet.org>
2022-08-08 12:10:50 +0200

    janky guest/admin test, need to theorize more

--- server/src/Helpers/Auth.ts
+++ server/src/Helpers/Auth.ts
@@ -38,3 +38,53 @@ export function Auth(req: express.Request, res: express.Response, next: express.
     }
 
 }
+
+export function AuthGuest(req: express.Request, res: express.Response, next: express.NextFunction): void {
+
+    /*
+    console.debug(`${req.ip} accessed ${req.path} with guest`);
+
+    // password protect
+    const password = Config.getInstance().cfg<string>("password");
+    const basepath = Config.getInstance().cfg<string>("basepath", "");
+
+    next();
+    */
+
+}
+
+export function AuthAdmin(req: express.Request, res: express.Response, next: express.NextFunction): void {
+
+    /*
+    // password protect
+    const password = Config.getInstance().cfg<string>("password");
+    const basepath = Config.getInstance().cfg<string>("basepath", "");
+
+    const ignored_paths = [
+        `${basepath}/api/v0/hook`,
+        `${basepath}/api/v0/cron/sub`,
+        `${basepath}/api/v0/cron/check_muted_vods`,
+        `${basepath}/api/v0/cron/check_deleted_vods`,
+        `${basepath}/api/v0/cron/playlist_dump`,
+    ];
+
+    if (Config.getInstance().cfg<string>("password", "") != "") {
+        if (ignored_paths.includes(req.path)) {
+            next();
+        } else {
+            const credentials = auth(req);
+            if (credentials && credentials.name == "admin" && credentials.pass == password) {
+                next();
+            } else {
+                console.log(chalk.bgRed.whiteBright(`${req.ip} attempted to access admin route '${req.path}' without proper credentials`));
+                res.statusCode = 401;
+                res.setHeader("WWW-Authenticate", `Basic realm="${AppName}"`);
+                res.end("Access denied");
+            }
+        }
+    } else {
+        next();
+    }
+    */
+
+}
