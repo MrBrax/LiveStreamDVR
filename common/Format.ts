1c804ab7e87609baa89d8b3dba10bb0d486535e0
braxen <braxen@braxnet.org>
2022-09-07 14:04:35 +0200

    extend formatString to hide empty, tests

--- common/Format.ts
+++ common/Format.ts
@@ -1,11 +1,14 @@
 // https://stackoverflow.com/questions/7975005/format-a-javascript-string-using-placeholders-and-an-object-of-substitutions
-export function formatString(string: string, replacements: Record<string, string>): string {
+export function formatString(string: string, replacements: Record<string, string>, hideEmpty = false): string {
     // return string.replace(/{(\d+)}/g, (match, number) => {
     //     return typeof args[number] !== "undefined" ? args[number] : match;
     // });
     return string.replace(
         /{(\w+)}/g, 
         (placeholderWithDelimiters, placeholderWithoutDelimiters) =>
+            hideEmpty ?
+            replacements[placeholderWithoutDelimiters] !== undefined ? replacements[placeholderWithoutDelimiters] : ""
+            :
             replacements[placeholderWithoutDelimiters] || placeholderWithDelimiters
     );
 }
