c8c7057f8ace46e2d1fd863fb08d369dd4c3c62b
braxen <braxen@braxnet.org>
2022-09-23 11:10:15 +0200

    try compositions api for no reason

--- client-vue/src/components/DurationDisplay.vue
+++ client-vue/src/components/DurationDisplay.vue
@@ -2,63 +2,59 @@
     <span>{{ timeString }}</span>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import { intervalToDuration, parseJSON } from "date-fns";
+<script lang="ts" setup>
+import { onMounted, onUnmounted, ref, watch } from "vue";
+import { parseJSON } from "date-fns";
+import { formatDuration, niceDuration, shortDuration } from "@/mixins/newhelpers";
 
-export default defineComponent({
-    name: "DurationDisplay",
-    props: {
-        startDate: { type: [String, Number], default: "0", },
-        outputStyle: { type: String, default: "human", }
-    },
-    data() {
-        return {
-            interval: 0,
-            timeString: "??:??",
-        };
-    },
-    watch: {
-        startDate(a, b) {
-            console.debug("DurationDisplay: startDate changed", a, b);
-            this.refreshTime();
-        },
-    },
-    mounted() {
-        this.refreshTime();
-        console.debug("Starting interval");
-        this.interval = window.setInterval(() => {
-            this.refreshTime();
-        }, 1000);
-    },
-    unmounted() {
-        if (this.interval) {
-            console.debug("Clearing interval");
-            clearTimeout(this.interval);
-        }
-    },
-    methods: {
-        refreshTime() {
-            if (!this.startDate || this.startDate == "0") return;
-            const dateObj = parseJSON(this.startDate);
-            // const dur = intervalToDuration({ start: dateObj, end: new Date() });
-            const totalSeconds = Math.abs(Math.floor((new Date().getTime() - dateObj.getTime()) / 1000));
-            if (this.outputStyle == "human") {
-                
-                // let str = "";
-                // if (dur.hours && dur.hours > 0) str += `${dur.hours}h `;
-                // if ((dur.minutes && dur.minutes > 0) || (dur.hours && dur.hours > 0)) str += `${dur.minutes}m `;
-                // if ((dur.seconds && dur.seconds > 0) || (dur.minutes && dur.minutes > 0)) str += `${dur.seconds}s `;
-                this.timeString = this.niceDuration(totalSeconds);
-            } else if(this.outputStyle == "humanLong") {
-                this.timeString = this.shortDuration(totalSeconds);
-            } else if(this.outputStyle == "numbers") {
-                this.timeString = this.formatDuration(totalSeconds);
-            } else {
-                this.timeString = "Invalid output style";
-            }
-            console.debug("updateTime", this.startDate, this.timeString);
-        },
-    },
+const props = defineProps({
+    startDate: { type: [String, Number], default: "0", },
+    outputStyle: { type: String, default: "human", }
 });
+
+const interval = ref(0);
+const timeString = ref("??:??");
+
+watch(() => props.startDate, (a, b) => {
+    console.debug("DurationDisplay: startDate changed", a, b);
+    refreshTime();
+});
+
+
+onMounted(() => {
+    refreshTime();
+    console.debug("Starting interval");
+    interval.value = window.setInterval(() => {
+        refreshTime();
+    }, 1000);
+});
+
+onUnmounted(() => {
+    if (interval.value) {
+        console.debug("Clearing interval");
+        clearTimeout(interval.value);
+    }
+});
+
+const refreshTime = () => {
+    if (!props.startDate || props.startDate == "0") return;
+    const dateObj = parseJSON(props.startDate);
+    // const dur = intervalToDuration({ start: dateObj, end: new Date() });
+    const totalSeconds = Math.abs(Math.floor((new Date().getTime() - dateObj.getTime()) / 1000));
+    if (props.outputStyle == "human") {
+        // let str = "";
+        // if (dur.hours && dur.hours > 0) str += `${dur.hours}h `;
+        // if ((dur.minutes && dur.minutes > 0) || (dur.hours && dur.hours > 0)) str += `${dur.minutes}m `;
+        // if ((dur.seconds && dur.seconds > 0) || (dur.minutes && dur.minutes > 0)) str += `${dur.seconds}s `;
+        timeString.value = niceDuration(totalSeconds);
+    } else if(props.outputStyle == "humanLong") {
+        timeString.value = shortDuration(totalSeconds);
+    } else if(props.outputStyle == "numbers") {
+        timeString.value = formatDuration(totalSeconds);
+    } else {
+        timeString.value = "Invalid output style";
+    }
+    console.debug("updateTime", props.startDate, timeString);
+};
+   
 </script>
