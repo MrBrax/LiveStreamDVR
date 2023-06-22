199de2abd304ef2a7822dc81d6f8e9642d1b97f2
Braxen <braxen@braxnet.org>
2023-06-22 15:53:09 +0200

    fix translations

--- server/src/Helpers/i18n.ts
+++ server/src/Helpers/i18n.ts
@@ -3,6 +3,7 @@ import i18nextMiddleware from "i18next-http-middleware";
 import messages_de from "../Translations/de.json";
 import messages_en from "../Translations/en.json";
 
+/*
 i18next.init({
     preload: ["en", "de"],
     fallbackLng: "en",
@@ -15,6 +16,7 @@ i18next.init({
         },
     },
 });
+*/
 
 i18next.use(i18nextMiddleware.LanguageDetector).init({
     preload: ["en", "de"],
@@ -37,5 +39,20 @@ i18next.on("languageChanged", (lng) => {
     console.log("Language changed to " + lng);
 });
 
+i18next.on("initialized", (options) => {
+    console.log(`Initialized with ${options}`);
+});
+
+i18next.on("loaded", (loaded) => {
+    console.log("Loaded " + loaded);
+});
+
+i18next.on("failedLoading", (lng, ns, msg) => {
+    console.log(`Failed loading ${lng} ${ns} ${msg}`);
+});
+
+i18next.on("missingKey", (lngs, namespace, key, res) => {
+    console.log(`Missing key ${key}`);
+});
 
 export default i18nextMiddleware.handle(i18next, {});
