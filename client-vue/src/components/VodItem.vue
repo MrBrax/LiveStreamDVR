bb8d8f409f5e33522a102e0474f76f91d9f4c4b8
CATERPILLAR\Braxen <braxen@braxnet.org>
2023-11-06 14:35:23 +0100

    fix vod id in item

--- client-vue/src/components/VodItem.vue
+++ client-vue/src/components/VodItem.vue
@@ -314,6 +314,7 @@ const renameVodTemplatePreview = computed(() => {
         absolute_episode: props.vod.stream_absolute_number ? props.vod.stream_absolute_number.toString().padStart(2, "0") : "",
         title: props.vod.getTitle() || "",
         game_name: isTwitchVOD(props.vod) ? (props.vod.current_game ? props.vod.current_game.name : "") : "",
+        game_id: isTwitchVOD(props.vod) ? (props.vod.current_game ? props.vod.current_game.id : "") : "",
     };
     const replaced_string = formatString(renameVodSettings.value.template, replacements);
     return replaced_string;
