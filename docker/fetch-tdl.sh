61a51aa85b6a2bdbe4db17362cfe5c24f1b55fec
CATERPILLAR\Braxen <braxen@braxnet.org>
2023-11-22 14:13:12 +0100

    update twitchdownloader version
    related to #481

--- docker/fetch-tdl.sh
+++ docker/fetch-tdl.sh
@@ -10,7 +10,7 @@
 set -euxo pipefail
 
 # set version
-TD_VERSION="1.53.0";
+TD_VERSION="1.53.5";
 
 # set arch, compatible with Linux-x64, LinuxArm, and LinuxArm64
 if [ "$(uname -m)" = "x86_64" ]; then
